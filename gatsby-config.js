bd415b6e5636f37d7979a41a34e13daf5ec4ef5e
Chajin Ahn <infilljean@gmail.com>
2022-07-24 21:45:44 +0900

    chore: update google analytics setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,41 +42,12 @@ module.exports = {
         },
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: metaConfig.ga,
-    //     head: true,
-    //     anonymize: true,
-    //   },
-    // },
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          metaConfig.ga, // Google Analytics / GA
-          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          origin: "YOUR_SELF_HOSTED_ORIGIN",
-        },
+        trackingId: metaConfig.ga,
+        head: true,
+        anonymize: true,
       },
     },
     {
